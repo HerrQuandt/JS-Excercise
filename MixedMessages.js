0ad398903ca9dd2d41512cc17557474aa132882f
Quandt <danielquandt@web.de>
2023-04-12 20:01:07 +0200

    finished the task

--- MixedMessages.js
+++ MixedMessages.js
@@ -1,26 +1,68 @@
-////////////////////
-//Arrays
-////////////////////
+const button = document.querySelector("#button")
 
-const startArray = [];
-const middleArray = [];
-const endArray = [];
+function randomStart(randomNumber) {
+    const startArr = ["Good morning ","Hello ","Good night "];
 
-const finalMessage = {
-
+    randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case 0:
+            return startArr[0];
+            break;
+        case 1:
+            return startArr[1];
+            break;
+        case 2:
+            return startArr[2];
+        default:
+            console.log("Go away ");
+    }
 }
 
-////////////////////
-//Read User Input
-////////////////////
+function randomMiddle(randomNumber) {
+    const midArr = ["Mr. ","Mrs. ", "Ms. "];
 
+    randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case 0:
+            return midArr[0];
+            break;
+        case 1:
+            return midArr[1];
+            break;
+        case 2:
+            return midArr[2];
+        default:
+            console.log("you filthy ");
+    }
+}
 
-////////////////////////////////////////////////////////////
-//Compare User Input with Position of Element in the Arrays
-////////////////////////////////////////////////////////////
+function randomEnd(randomNumber) {
+    const endArr = ["Quandt","Merkel","Bond"];
 
+    randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case 0:
+            return endArr[0];
+            break;
+        case 1:
+            return endArr[1];
+            break;
+        case 2:
+            return endArr[2];
+        default:
+            console.log("casual!");
+    }
+}
 
-////////////////////////////////////////
-//Combine the chosen Array Elements
-//into one single string and output it
-////////////////////////////////////////
+const randomSentence = {
+    greeting: randomStart(),
+    salutation: randomMiddle(),
+    name: randomEnd(),
+};
+
+// function createSentence() {
+//     document.getElementById("h1").innerHTML = (randomSentence.greeting + randomSentence.salutation + randomSentence.name)
+// };
+
+// button.addEventListener("click", createSentence);
+document.getElementById("h1").innerHTML = (randomSentence.greeting + randomSentence.salutation + randomSentence.name);
